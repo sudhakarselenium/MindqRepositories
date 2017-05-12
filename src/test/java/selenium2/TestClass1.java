4fb5480f1c652d984c4217b7ee82e8b8436cac99
sudhakarselenium <me@qtpsudhakar.com>
2017-05-12 08:30:14 +0530

    remote update

--- src/test/java/selenium2/TestClass1.java
+++ src/test/java/selenium2/TestClass1.java
@@ -10,6 +10,7 @@ public class TestClass1 {
 		System.out.println("update 5");
 		System.out.println("update 6");
 		System.out.println("again update");
+		System.out.println("Remote update");
 	}
 
 }
